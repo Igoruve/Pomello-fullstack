f87a348849e6d6f0660d77b8d6031418702a20db
igoruve <igoruve@gmail.com>
2025-05-15 15:24:10 +0200

    project fetch done

--- client/src/utils/project.js
+++ client/src/utils/project.js
@@ -1,40 +1,18 @@
 import FetchData from "./fetch.js";
-import projectsData from "../data/projectsData.js";
-import usersData from "../data/usersData.js";
 
 const getAllProjects = async () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(projectsData);
-    }, 500);
-  });
+  const projects = await FetchData("/projects");
+  return projects;
 };
 
 const getProjectById = async (id) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const project = projectsData.find((project) => project._id.$oid === id);
-      resolve(project);
-    }, 500);
-  });
+  const project = await FetchData(`/projects/${id}`);
+  return project;
 };
 
 const getProjectsByUserId = async (userId) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const user = usersData.find((u) => u._id.$oid === userId);
-      
-      if (!user) return resolve([]);
-
-      const userProjectIds = user.projects.map((proj) => proj.$oid);
-
-      const userProjects = projectsData.filter((proj) =>
-        userProjectIds.includes(proj._id.$oid)
-      );
-
-      resolve(userProjects);
-    }, 500);
-  });
+  const userProjects = await FetchData(`/project/user/${userId}`);
+  return userProjects;
 };
 
 export { getAllProjects, getProjectById, getProjectsByUserId };
--- client/src/utils/user.js
+++ client/src/utils/user.js
@@ -1,20 +1,18 @@
 import usersData from "../data/usersData";
+import FetchData from "../data/FetchData";
 
 const getAllUsers = async () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(usersData);
-        }, 500);
-    });
+  const users = await FetchData("/users");
+  return users;
 };
 
 const getUserById = async (id) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const userItem = usersData.find((user) => user._id.$oid === id);
-            resolve(userItem);
-        }, 500);
-    });
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const userItem = usersData.find((user) => user._id.$oid === id);
+      resolve(userItem);
+    }, 500);
+  });
+};
 
 export { getAllUsers, getUserById };
