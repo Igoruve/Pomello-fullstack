13a8f73d9f6631bc9a5a78957fb016862abef869
Dalila <crdalila@gmail.com>
2025-05-16 19:43:23 +0200

    lets make a new endpoint

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,26 +1,14 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 function Project() {
   const loaderData = useLoaderData();
-
-  if (!loaderData) return <div className="text-white">Loading...</div>;
-
-  if (!Array.isArray(loaderData)) {
-    return <div className="text-white">Error: {loaderData?.message || "Unexpected error"}</div>;
-  }
+  const { id: projectId } = useParams();
 
   return (
-    <section>
-      <h1 className="text-white">{loaderData.title}</h1>
-      <section>
-        {loaderData.map((list) => (
-          <div key={list._id.$oid}>
-            <h2 className="text-white">{list.title}</h2>
-
-          </div>
-        ))}
-      </section>
+    <section className="text-white">
+      <h2>Proyecto</h2>
     </section>
+ 
   );
 }
 
