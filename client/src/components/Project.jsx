70dd59d6cdeaf745c560726585bb59e7107b0b49
Dalila <crdalila@gmail.com>
2025-05-16 20:21:38 +0200

    projects now give us the project and list and tasksgit add .git add .

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,11 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 
 function Project() {
-  const data = useLoaderData();
+  const project = useLoaderData();
 
   return (
     <section className="text-white">
-      <h2>{data.project.title}</h2>
+      <h2>{project.title}</h2>
+      {project.lists.map((list) => (
+        <div>
+          <h3>{list.title}</h3>
+          <ul className="list-none text-red-500">
+            {list.tasks.map((task) => (
+              <li>{task.title}</li>
+            ))}
+          </ul>
+        </div>
+      ))} 
     </section>
 
   );
