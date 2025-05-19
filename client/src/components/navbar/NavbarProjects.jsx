da0f6634f92ca27b7afb7659b9262434d8a49973
Dalila <crdalila@gmail.com>
2025-05-19 18:51:58 +0200

    new icon and colors fixed

--- client/src/components/navbar/NavbarProjects.jsx
+++ client/src/components/navbar/NavbarProjects.jsx
@@ -29,7 +29,7 @@ function NavbarProjects() {
         <section className="my-4">
             <div className="flex flex-row justify-between items-center mb-4">
                 <p>Projects</p>
-                <svg onClick={() => setExpanded(!expanded)} className="text-white" viewBox="0 0 448 512" fill="white" height="18px" width="18px"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" /></svg>
+                <svg onClick={() => setExpanded(!expanded)} className="text-white" viewBox="0 0 448 512" fill="#f56b79" height="18px" width="18px"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" /></svg>
             </div>
 
             <section>
@@ -46,7 +46,7 @@ function NavbarProjects() {
                                     <textarea className="bg-gray-800 border border-white/50 rounded-sm" type="text" name="description" id="description" />
                                 </div>
                             </article>
-                            <button className="bg-amber-500 py-3 px-1.5 rounded-xl" type="submit">Create</button>
+                            <button className="bg-[#e84860] py-3 px-1.5 rounded-xl" type="submit">Create</button>
                         </form>
                     </div>
                 )}
@@ -55,7 +55,7 @@ function NavbarProjects() {
             <div>
                 {loaderData.map((project) => (
                     <Link to={`/project/${project._id}`} key={project._id}>
-                        <div className="mb-2 bg-gray-600 rounded-2xl p-4 cursor-pointer hover:bg-amber-500">
+                        <div className="mb-2 bg-gray-600 rounded-2xl p-4 cursor-pointer hover:bg-[#e84860]">
                             <p>{project.title}</p>
                         </div>
                     </Link>
--- client/src/components/navbar/TopNavbar.jsx
+++ client/src/components/navbar/TopNavbar.jsx
@@ -6,10 +6,14 @@ function TopNavbar() {
   return (
     <section className="flex flex-row bg-gray-800 border border-b-gray-200/20 h-18 w-screen">
       <nav className="flex flex-row gap-4 text-white/80 w-full justify-between items-center px-12 text-lg ">
-        <img src="/assets/icon_02.png" alt="pomello icon" className="h-8 w-auto" />
-
-        <p>Browser</p>
-        <p>User</p>
+        <div className="flex flex-row items-center gap-2">
+          <img src="/assets/icon_01.png" alt="pomello icon" className="h-8 w-auto" />
+          <h1 className="font-bold text-white/80 text-2xl">POMELLO</h1>
+        </div>
+        <div className="flex flex-row items-center gap-4">
+          <p>Browser</p>
+          <p>User</p>
+        </div>
       </nav>
     </section>
   );
