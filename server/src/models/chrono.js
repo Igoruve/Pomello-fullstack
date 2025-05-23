0b8188177967878d49bf2f9e72337e20f4670853
Dalila <crdalila@gmail.com>
2025-05-23 20:28:49 +0200

    new commit with chrono to dev

---
+++ server/src/models/chrono.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const ChronoSchema = new mongoose.Schema({
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    //required: true 
+  },
+  focusDuration: { 
+    type: Number, 
+    required: true 
+  }, 
+  breakDuration: { 
+    type: Number, 
+    required: true 
+  }, 
+  chronostarted: { 
+    type: Date, 
+    default: Date.now,
+    required: true 
+  },
+  chronostopped: {
+    type: Date, 
+    default: Date.now,
+    //required: true
+  }, 
+  sessionsCompleted: { 
+    type: Number, 
+    default: 0 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  } 
+});
+
+export default mongoose.model('Chrono', ChronoSchema);
---
+++ server/src/routes/chronoRouter.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import chronoController from '../controllers/chronoController.js';
+import isLoggedInAPI from '../middlewares/authMiddleware.js';
+
+
+
+const router = Router();
+
+//router.post('/start', isLoggedInAPI, chronoController.startChrono);
+
+//router.post('/stop', isLoggedInAPI, chronoController.stopChrono);
+
+router.get('/stats', isLoggedInAPI, chronoController.getChronoStats);
+
+router.post('/pomellodoro/start', isLoggedInAPI, chronoController.startPomellodoroCycle);
+
+router.post('/pomellodoro/stop', isLoggedInAPI, chronoController.stopPomellodoroCycle);
+
+router.get('/pomellodoro/status', isLoggedInAPI, chronoController.getPomellodoroStatus);
+
+export default router;
